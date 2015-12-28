e0f0f7e66c69b0ba230fc85e417294f4de8e5f30
Kirill Leyfer <kolbasisha@gmail.com>
2015-12-28 17:05:33 +0600

    Copy buffer instead of passing it directly to asyncTask.

--- services/core/java/com/android/server/policy/touchlogger/TouchSaver.java
+++ services/core/java/com/android/server/policy/touchlogger/TouchSaver.java
@@ -3,11 +3,13 @@ package com.android.server.policy.touchlogger;
 import com.android.server.policy.touchlogger.helper.GestureBuffer;
 import com.android.server.policy.touchlogger.task.SaveGesturesTask;
 
+import java.util.ArrayList;
+
 public class TouchSaver {
     private static TouchSaver ourInstance = new TouchSaver();
     private final int maxBufferSize = 2 << 17;
 
-    private GestureBuffer gestureBuffer = new GestureBuffer();
+    private GestureBuffer gestureBuffer = new GestureBuffer(new ArrayList<String>(), 0);
 
     public static TouchSaver getInstance() {
         return ourInstance;
@@ -19,7 +21,7 @@ public class TouchSaver {
     public void saveGesture(String gesture) {
         gestureBuffer.addGesture(gesture);
         if (gestureBuffer.getBufferSize() >= maxBufferSize) {
-            new SaveGesturesTask().doInBackground(gestureBuffer);
+            new SaveGesturesTask().doInBackground(new GestureBuffer(gestureBuffer));
             gestureBuffer.clear();
         }
     }
--- services/core/java/com/android/server/policy/touchlogger/helper/GestureBuffer.java
+++ services/core/java/com/android/server/policy/touchlogger/helper/GestureBuffer.java
@@ -4,27 +4,40 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class GestureBuffer {
-    private List<String> mGestures = new ArrayList<String>();
-    private int bufferSize = 0;
+    private List<String> mGesturesList;
+    private int mBufferSize;
+
+    public GestureBuffer(List<String> gesturesList, int bufferSize) {
+        mGesturesList = new ArrayList<String>(gesturesList);
+        mBufferSize = bufferSize;
+    }
+
+    public GestureBuffer(GestureBuffer buffer) {
+        this(buffer.getGesturesList(), buffer.getBufferSize());
+    }
 
     public void addGesture(String gesture) {
-        mGestures.add(gesture);
-        bufferSize += gesture.length();
+        mGesturesList.add(gesture);
+        mBufferSize += gesture.length();
+    }
+
+    public List<String> getGesturesList() {
+        return mGesturesList;
     }
 
     public int getBufferSize() {
-        return bufferSize;
+        return mBufferSize;
     }
 
     public void clear() {
-        bufferSize = 0;
-        mGestures.clear();
+        mBufferSize = 0;
+        mGesturesList.clear();
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
-        for (String gesture: mGestures) {
+        for (String gesture: mGesturesList) {
             sb.append(gesture);
         }
         return sb.toString();
